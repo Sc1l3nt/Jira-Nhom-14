8d44a7363c6765d4de3776e3c6e4686d8cbd006f
tminhbao <tminhbao2001@gmail.com>
2023-02-04 11:07:29 +0700

    create reducers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  Navigate,
   Route,
   Routes,
   unstable_HistoryRouter as HistoryRouter,
--- src/redux/configStore.jsx
+++ src/redux/configStore.jsx
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import commentReducer from "./reducers/commentReducer";
+import projectReducer from "./reducers/projectReducer";
+import taskReducer from "./reducers/taskReducer";
+import userReducer from "./reducers/userReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    commentReducer: commentReducer,
+    projectReducer: projectReducer,
+    taskReducer: taskReducer,
+    userReducer: userReducer,
+  },
 });
