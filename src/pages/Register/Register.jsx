b1674f6e06931ec4653317716ae21b2bdd036a25
tminhbao <tminhbao2001@gmail.com>
2023-02-06 02:01:01 +0700

    create register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import FormInput from "../../components/FormInput/FormInput";
 
 const Register = () => {
-  return <div>Register</div>;
+  let listRender = ["email", "password", "comfirm", "name", "phone", "login"];
+  return (
+    <FormInput
+      listRender={listRender}
+      textButton="Register"
+      link={
+        <NavLink className="mt-2" to="/login">
+          I already have an account
+        </NavLink>
+      }
+    />
+  );
 };
 
 export default Register;
