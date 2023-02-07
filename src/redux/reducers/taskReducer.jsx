53a1b1d2b20e611bda07f7d092cc910eceb027bb
tminhbao <tminhbao2001@gmail.com>
2023-02-08 01:23:13 +0700

    update statusReducer

--- src/redux/reducers/taskReducer.jsx
+++ src/redux/reducers/taskReducer.jsx
@@ -14,55 +14,19 @@ const taskReducer = createSlice({
     getAllTaskTypesAction: (state, action) => {
       state.taskTypes = action.payload;
     },
-    updateTaskStatusAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
     getTaskDetailAction: (state, action) => {
       state.taskDetail = action.payload;
     },
-    createTaskAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    updateTaskAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    updateDescriptionAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    updatePriorityAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    assignUserToTaskAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    removeUserFromTaskAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    updateEstimateAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    updateTimeTrackingAction: (state, action) => {
-      state.taskDetail = action.payload;
-    },
-    removeTaskAction: (state, action) => {
-      state.taskDetail = action.payload;
+    setTaskErrorAction: (state, action) => {
+      state.taskError = action.payload;
     },
   },
 });
 
 export const {
   getAllTaskTypesAction,
-  updateTaskStatusAction,
   getTaskDetailAction,
-  createTaskAction,
-  updateTaskAction,
-  updateDescriptionAction,
-  updatePriorityAction,
-  assignUserToTaskAction,
-  removeUserFromTaskAction,
-  updateEstimateAction,
-  updateTimeTrackingAction,
-  removeTaskAction,
+  setTaskErrorAction,
 } = taskReducer.actions;
 
 export default taskReducer.reducer;
@@ -77,12 +41,10 @@ export const getAllTaskTypesApi = () => {
 
 export const updateTaskStatusApi = ({ taskId, statusId }) => {
   return async (dispatch) => {
-    const result = await http.put(`/Project/updateStatus`, {
+    await http.put(`/Project/updateStatus`, {
       taskId,
       statusId,
     });
-    const action = updateTaskStatusAction(result.data.content);
-    dispatch(action);
   };
 };
 
@@ -90,72 +52,66 @@ export const getTaskDetailApi = (taskId) => {};
 
 export const createTaskApi = (data) => {
   return async (dispatch) => {
-    const result = await http.post(`/Project/createTask`, data);
-    const action = createTaskAction(result.data.content);
-    dispatch(action);
+    await http.post(`/Project/createTask`, data);
   };
 };
 
 export const updateTaskApi = (data) => {
   return async (dispatch) => {
-    const result = await http.post(`/Project/updateTask`, data);
-    const action = updateTaskAction(result.data.content);
-    dispatch(action);
+    await http.post(`/Project/updateTask`, data);
   };
 };
 
 export const updateDescriptionApi = ({ taskId, description }) => {
   return async (dispatch) => {
-    const result = await http.put(`/Project/updateDescription`, {
+    await http.put(`/Project/updateDescription`, {
       taskId,
       description,
     });
-    const action = updateDescriptionAction(result.data.content);
-    dispatch(action);
   };
 };
 
 export const updatePriorityApi = ({ taskId, priorityId }) => {
   return async (dispatch) => {
-    const result = await http.put(`/Project/updateStatus`, {
+    await http.put(`/Project/updateStatus`, {
       taskId,
       priorityId,
     });
-    const action = updatePriorityAction(result.data.content);
-    dispatch(action);
   };
 };
 
 export const assignUserToTaskApi = ({ taskId, userId }) => {
   return async (dispatch) => {
-    const result = await http.post(`/Project/assignUserTask`, {
+    await http.post(`/Project/assignUserTask`, {
       taskId,
       userId,
     });
-    const action = assignUserToTaskAction(result.data.content);
-    dispatch(action);
   };
 };
 
 export const removeUserFromTaskApi = ({ taskId, userId }) => {
   return async (dispatch) => {
-    const result = await http.post(`/Project/removeUserFromTask`, {
-      taskId,
-      userId,
-    });
-    const action = removeUserFromTaskAction(result.data.content);
-    dispatch(action);
+    try {
+      await http.post(`/Project/removeUserFromTask`, {
+        taskId,
+        userId,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 export const updateEstimateApi = ({ taskId, originalEstimate }) => {
   return async (dispatch) => {
-    const result = await http.put(`/Project/updateEstimate`, {
-      taskId,
-      originalEstimate,
-    });
-    const action = updateEstimateAction(result.data.content);
-    dispatch(action);
+    try {
+      await http.put(`/Project/updateEstimate`, {
+        taskId,
+        originalEstimate,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 };
 
@@ -165,20 +121,24 @@ export const updateTimeTrackingApi = ({
   timeTrackingRemaining,
 }) => {
   return async (dispatch) => {
-    const result = await http.put(`/Project/updateTimeTracking`, {
-      taskId,
-      timeTrackingSpent,
-      timeTrackingRemaining,
-    });
-    const action = updateTimeTrackingAction(result.data.content);
-    dispatch(action);
+    try {
+      await http.put(`/Project/updateTimeTracking`, {
+        taskId,
+        timeTrackingSpent,
+        timeTrackingRemaining,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 export const removeTaskApi = (taskId) => {
   return async (dispatch) => {
-    const result = await http.delete(`/Project/removeTask?taskId=${taskId}`);
-    const action = removeTaskAction(result.data.content);
-    dispatch(action);
+    try {
+      await http.delete(`/Project/removeTask?taskId=${taskId}`);
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
